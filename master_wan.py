1262f9f0fd316fd7a129ef8bb60e5630a58e26bd
JorgeNavarro <jorge.85navarro@gmail.com>
2026-06-10 02:44:11 -0600

    Saltillo changes Jorge

--- master_wan.py
+++ master_wan.py
@@ -4,44 +4,22 @@ from mininet.link import TCLink
 from mininet.cli import CLI
 from mininet.log import setLogLevel
 
-## Import our modular site topologies
-
+## Sites
+from illinois import Illinois
 from saltillo import Saltillo
-from Router import Router
 
 def build_master():
-    net = Mininet(controller=None, switch=OVSSwitch, link=TCLink)
+    net = Mininet(controller=None, switch=OVSSwitch, link=TCLink, autoSetMacs=True)
 
     saltillo = Saltillo()
     saltillo.build(net)
 
-    r2 = net.addHost('r2', cls=Router)
-    r3 = net.addHost('r3', cls=Router)
+    illinois = Illinois()
+    illinois.build()
 
-    net.addLink(saltillo.gateway,r2)
-    net.addLink(saltillo.gateway,r3)
+    net.addLink(illinois.gateway, saltillo.gateway, intfName1='illi-sal', intfName2='sal-illi', cls=TCLink)
 
     net.start()
-    r1, r4 = saltillo.gateway, saltillo.gateway
-
-    r1.setIP('172.16.1.1/30', intf='r1-eth1')
-    r1.setIP('172.16.2.1/30', intf='r1-eth2')
-    r4.setIP('172.16.1.6/30', intf='r4-eth1')
-    r4.setIP('172.16.2.6/30', intf='r4-eth2')
-
-    r2.setIP('172.16.1.2/30', intf='r2-eth0')
-    r2.setIP('172.16.1.5/30', intf='r2-eth1')
-    r3.setIP('172.16.2.2/30', intf='r3-eth0')
-    r3.setIP('172.16.2.5/30', intf='r3-eth1')
-
-    r2.cmd('ip route replace 10.0.1.0/24 nexthop via 172.16.1.1 dev r2-eth0')
-    r2.cmd('ip route replace 10.0.2.0/24 nexthop via 172.16.1.6 dev r2-eth1')
-
-    r3.cmd('ip route replace 10.0.1.0/24 nexthop via 172.16.2.1 dev r3-eth0')
-    r3.cmd('ip route replace 10.0.2.0/24 nexthop via 172.16.2.6 dev r3-eth1')
-
-    r1.cmd('ip route replace 10.0.2.0/24 nexthop via 172.16.1.2 dev r1-eth1 nexthop via 172.16.2.2 dev r1-eth2')
-    r4.cmd('ip route replace 10.0.1.0/24 nexthop via 172.16.1.5 dev r4-eth1 nexthop via 172.16.2.5 dev r4-eth2')
 
     CLI(net)
     net.stop()
--- saltillo.py
+++ saltillo.py
@@ -3,6 +3,7 @@ from mininet.node import OVSSwitch
 from mininet.link import TCLink
 from mininet.cli import CLI
 from mininet.log import setLogLevel
+from base_nodes import SwitchL3, Router
 
 # Saltillo site
 
@@ -11,40 +12,40 @@ from mininet.log import setLogLevel
 # 3 Distribution switch
 # 11 Access switches
 
-from base_nodes import SwitchL3, Router
 
 class Saltillo:
+
     def __init__(self):
         self.gateway = None
 
     def build(self, net):
-        self.gateway = net.addHost('saltillo', cls=Router, ip='10.30.255.254/16')
-
+        self.gateway = net.addHost('saltillo', cls=Router)
         sSALc1 = net.addSwitch('sSALc1', cls=SwitchL3, failMode='standalone') # Core switch
+        net.addLink(sSALc1, self.gateway)
 
+                # Distribution switch
         sSALd1 = net.addSwitch('sSALd1', failMode='standalone') ## Floor 1 & lobby
         sSALd2 = net.addSwitch('sSALd2', failMode='standalone') ## Floor 2 
         sSALd3 = net.addSwitch('sSALd3', failMode='standalone') ## Floor 3
 
         ## FLOOR 1
-        #s_lpoe12 = net.addSwitch('sSALLlPoE12', failMode='standalone') ## Lobby PoE switch 
+        sSALl = net.addSwitch('sSALl', failMode='standalone') # Lobby PoE switch
         sSALf11 = net.addSwitch('sSALf11',failMode='standalone') ## Floor 1 non-PoE 48P (1)
         sSALf12 = net.addSwitch('sSALf12',failMode='standalone') # Floor 1 Non-PoE 48P (2)
         sSALf13 = net.addSwitch('sSALf13', failMode='standalone') # Floor 1 PoE 48P
-        
+
         ##FLOOR 2 
         sSALf21 = net.addSwitch('sSALf21', failMode='standalone') # Floor 2 Non-PoE 24P
         sSALf22 = net.addSwitch('sSALf22', failMode='standalone') # Floor 2 Non-PoE 48P (1)
         sSALf23 = net.addSwitch('sSALf23', failMode='standalone') # Floor 2 Non-PoE 48P (2)
         sSALf24 = net.addSwitch('sSALf24', failMode='standalone')  #Floor 2 PoE 12P
-
         
         ##FLOOR 3 
         sSALf31 = net.addSwitch('sSALf31', failMode='standalone') # Floor 3 Non-PoE 24P
         sSALf32 = net.addSwitch('sSALf32', failMode='standalone') # Floor 3 PoE 24P
         sSALf33 = net.addSwitch('sSALf33', failMode='standalone') # Floor 3 Non-PoE 48P
 
-        #-Linking process
+        # Linking process of switches
         
         net.addLink(self.gateway, sSALc1) # Core to router
 
@@ -52,19 +53,19 @@ class Saltillo:
         net.addLink(sSALc1, sSALd2)
         net.addLink(sSALc1, sSALd3)
 
-        #net.addLink(sSALDis1,sSALLlPoE12) # Distribution to access floor 1
+        net.addLink(sSALd1,sSALl) # Distribution to access floor 1
         net.addLink(sSALd1,sSALf11)
-        #net.addLink(sSALDis1,sSALLf1NPoE482)
-        #net.addLink(sSALDis1,sSALLf1PoE48)
+        net.addLink(sSALd1,sSALf12)
+        net.addLink(sSALd1,sSALf13)
 
-        #net.addLink(sSALDis2,sSALLf2poe12) # Distribution to access floor 2
-        net.addLink(sSALd2,sSALf21)
-        #net.addLink(sSALDis2,sSALLf2npoe481)
-        #net.addLink(sSALDis2,sSALLf2npoe482)
+        net.addLink(sSALd2,sSALf21) # Distribution to access floor 2
+        net.addLink(sSALd2,sSALf22)
+        net.addLink(sSALd2,sSALf23)
+        net.addLink(sSALd2,sSALf24)
 
-        #net.addLink(sSALDis3,sSALLf3poe24) # Distribution to access floor 3
-        net.addLink(sSALd3,sSALf31)
-        #net.addLink(sSALDis3,sSALLf3npoe48)
+        net.addLink(sSALd3,sSALf31) # Distribution to access floor 3
+        net.addLink(sSALd3,sSALf32)
+        net.addLink(sSALd3,sSALf33) 
 
         # Representative hosts
 
@@ -73,29 +74,39 @@ class Saltillo:
         hSALf2eng1 = net.addHost('hSALf2eng1', ip='10.30.20.20/25')
         hSALf3eng1 = net.addHost('hSALf3eng1', ip='10.30.20.30/25')
 
-        ## Representative nodes reception  - VLAN 50        
-        hSALf1rec1 = net.addHost('hSALf1rec1', ip='10.30.50.10/27')
-        hSALf2rec1 = net.addHost('hSALf2rec1', ip='10.30.50.20/27')
-        hSALf3rec1 = net.addHost('hSALf3rec1', ip='10.30.50.30/27')
-
         net.addLink(hSALf1eng1, sSALf11)
         net.addLink(hSALf2eng1, sSALf21)
         net.addLink(hSALf3eng1, sSALf31)
+
+        ## Representative nodes reception  - VLAN 50        
+        hSALlrec1 = net.addHost('hSALlrec1', ip=None)
+        hSALf1rec1 = net.addHost('hSALf1rec1', ip=None)
+        hSALf2rec1 = net.addHost('hSALf2rec1', ip='10.30.50.20/27')
+        hSALf3rec1 = net.addHost('hSALf3rec1', ip='10.30.50.30/27')
+
+        net.addLink(hSALlrec1, sSALl)
         net.addLink(hSALf1rec1, sSALf11)
         net.addLink(hSALf2rec1, sSALf21)
         net.addLink(hSALf3rec1, sSALf31)
 
-        net.start()
         print("Saltillo site built successfully!")
+
+    def config(self, net):
+        
+            
 
         CLI(net)
         net.stop()
+    
+    
 
 
 def run():
-    net = Mininet(controller=None, switch=OVSSwitch, link=TCLink)
+    net = Mininet(controller=None, switch=OVSSwitch, link=TCLink, autoSetMacs=True)
     site = Saltillo()
     site.build(net)
+    net.start()
+    site.config(net)
 
 if __name__ == '__main__':
     setLogLevel('info')
